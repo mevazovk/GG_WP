0990759057cd44147946209e4114aaa7b86b764a
mevazovk <mevazov_k@auca.kg>
2024-11-19 12:56:01 +0600

    Final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
+import { useEffect, useState } from "react";
 import { ProductCard } from "./components/ProductCard";
 
-const products = [
-  {
-    id: 0,
-    name: "Product 1",
-    category: "Category 1",
-    price: 200,
-    img: "https://img.freepik.com/premium-photo/dog-field-flowers_865967-16274.jpg",
-  },
-  {id: 1,
-    name: "Product 2",
-    category: "Category 2",
-    price: 500,
-    img: "https://yac-wh-sb-prod-s3-media-07001.storage.yandexcloud.net/media/images/britanskaya-korotkosherstnaya-.max-2880x1820.format-jpeg_aw71Muw.jpg",
-  },
-];
-
 function App() {
+  const [loading, setLoading] = useState([true]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(()=> setLoading(false))
+  }, []);
+
   return (
     <div>
+      {loading && <div>Loading...</div>}
       {
         products.map((product)=>(
         <ProductCard data= {product}/>
